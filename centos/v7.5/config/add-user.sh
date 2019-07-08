5c252a52f6ee075c26c39848bf89bd18838e3792
hanmin <hanmin>
2019-07-08 10:39:09 +0900

    update README.md

--- centos/v7.5/config/add-user.sh
+++ centos/v7.5/config/add-user.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 
-## add ecube
-useradd -m -s /bin/bash -p ecube ecube
-cp -pr /home/vagrant/.ssh /home/ecube/
-chown -R ecube:ecube /home/ecube
-echo "%ecube ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ecube
+USER=$1
+
+## add user
+useradd -m -s /bin/bash -p $USER $USER
+cp -pr /home/vagrant/.ssh /home/$USER/
+chown -R $USER:$USER /home/$USER
+echo "%$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USER
 
 # install open jdk
 yum install java-1.8.0-openjdk-devel -y
